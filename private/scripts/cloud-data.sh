f1dde61ec7551639633a0937ab7b2d8e9013cf91
Paul Gomersbach <p.gomersbach@rely.nl>
2013-03-20 18:26:19 +0100

    add cloud-data script

---
+++ private/scripts/cloud-data.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+role=init
+puppet_source=https://code.google.com/p/demo-puppet-repo/
+set -e -x
+
+apt-get --yes --quiet update
+apt-get --yes --quiet install git puppet-common
+
+#
+# Fetch puppet configuration from public git repository.
+#
+
+mv /etc/puppet /etc/puppet.orig
+git clone $puppet_source /etc/puppet
+
+#
+# Run puppet.
+#
+
+puppet apply /etc/puppet/manifests/$role.pp
