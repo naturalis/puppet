6d03c83cdabe0825a72162eb326f092504fd9e2d
Paul Gomersbach <p.gomersbach@rely.nl>
2013-03-20 18:54:52 +0100

    add motd modules

--- private/scripts/cloud-data.sh
+++ private/scripts/cloud-data.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 role=init
 puppet_source=https://code.google.com/p/demo-puppet-repo/
-set -e -x
 
+
+set -e -x
+wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb
+dpkg -i puppetlabs-release-precise.deb
 apt-get --yes --quiet update
-apt-get --yes --quiet install git puppet-common
+apt-get --yes -o Dpkg::Options::="--force-confold" --quiet install git puppet-common
 
 #
 # Fetch puppet configuration from public git repository.
