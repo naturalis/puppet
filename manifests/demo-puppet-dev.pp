a48d5e5b4b0747fcc7a587400e2bae27240c925b
Paul Gomersbach <p.gomersbach@rely.nl>
2013-03-26 23:11:13 +0100

    add pre-commit hook

--- manifests/demo-puppet-dev.pp
+++ manifests/demo-puppet-dev.pp
@@ -2,7 +2,13 @@ class { 'git': }
 class { 'lockdown': }
 class { 'motd': }
 
-exec {'get_puppetdoc':
+exec {'get_demo-puppet-repo':
   command => '/usr/bin/git clone https://code.google.com/p/demo-puppet-repo/ /root/demo-puppet-repo',
   creates => '/root/demo-puppet-repo',
+}
+
+exec {'install-pre-commit-hook':
+  command => '/bin/cp /root/demo-puppet-repo/private/scripts/pre-commit /root/demo-puppet-repo/.git/hooks/pre-commit',
+  creates => '/root/demo-puppet-repo/.git/hooks/pre-commit',
+  require => Exec['get_demo-puppet-repo'],
 }
