c739b5e12f7264d736df15f88f9de26231c487b0
Paul Gomersbach <p.gomersbach@rely.nl>
2013-03-26 22:54:02 +0100

    Clone repo

--- manifests/demo-puppet-dev.pp
+++ manifests/demo-puppet-dev.pp
@@ -1,3 +1,8 @@
 class { 'git': }
 class { 'lockdown': }
 class { 'motd': }
+
+exec {'get_puppetdoc':
+  command => '/usr/bin/git clone https://code.google.com/p/demo-puppet-repo/ /root/demo-puppet-repo',
+  creates => '/root/demo-puppet-repo',
+}
