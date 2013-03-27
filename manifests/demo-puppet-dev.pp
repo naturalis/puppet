27d0a80a9bf06507f190529d15b2f80322ef28ec
Paul Gomersbach <p.gomersbach@rely.nl>
2013-03-27 11:10:08 +0100

    add puppet-lint

--- manifests/demo-puppet-dev.pp
+++ manifests/demo-puppet-dev.pp
@@ -2,14 +2,20 @@ class { 'git': }
 class { 'lockdown': }
 class { 'motd': }
 
-exec {'install-pre-commit-hook':
-  command => '/bin/cp /root/demo-puppet-repo/private/scripts/pre-commit /root/demo-puppet-repo/.git/hooks/pre-commit',
-  creates => '/root/demo-puppet-repo/.git/hooks/pre-commit',
-  require => Vcsrepo['/root/demo-puppet-repo'],
-}
-
 vcsrepo { '/root/demo-puppet-repo':
   ensure   => present,
   provider => git,
   source   => 'https://code.google.com/p/demo-puppet-repo/',
 }
+
+package { 'puppet-lint':
+  ensure  => installed,
+  require => Vcsrepo['/root/demo-puppet-repo'],
+}
+
+exec {'install-pre-commit-hook':
+  command => '/bin/cp /root/demo-puppet-repo/private/scripts/pre-commit /root/demo-puppet-repo/.git/hooks/pre-commit',
+  creates => '/root/demo-puppet-repo/.git/hooks/pre-commit',
+  require => Package['puppet-lint'],
+}
+
