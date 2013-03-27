f58c7f7044764b253220912e7244d73e1fcf565d
root <root@mydevinstance.(none)>
2013-03-27 10:01:30 +0000

    change from git module to vcsrepo

--- manifests/demo-puppet-dev.pp
+++ manifests/demo-puppet-dev.pp
@@ -2,13 +2,14 @@ class { 'git': }
 class { 'lockdown': }
 class { 'motd': }
 
-exec {'get_demo-puppet-repo':
-  command => '/usr/bin/git clone https://code.google.com/p/demo-puppet-repo/ /root/demo-puppet-repo',
-  creates => '/root/demo-puppet-repo',
-}
-
 exec {'install-pre-commit-hook':
   command => '/bin/cp /root/demo-puppet-repo/private/scripts/pre-commit /root/demo-puppet-repo/.git/hooks/pre-commit',
   creates => '/root/demo-puppet-repo/.git/hooks/pre-commit',
-  require => Exec['get_demo-puppet-repo'],
+  require => Vcsrepo['/root/demo-puppet-repo'],
+}
+
+vcsrepo { '/root/demo-puppet-repo':
+  ensure   => present,
+  provider => git,
+  source   => 'https://code.google.com/p/demo-puppet-repo/',
 }
