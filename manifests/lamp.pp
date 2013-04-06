f5d11199b09feaea00f4078ca9dc0b671589d5ad
root <root@development.(none)>
2013-04-06 15:13:32 +0000

    Add docroot

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -6,8 +6,8 @@ apache::vhost { 'www.example.com':
         priority        => '10',
         vhost_name      => '192.0.2.1',
         port            => '8000',
-        docroot         => '/home/www.example.com/docroot/',
-        logroot         => '/srv/www.example.com/logroot/',
+        docroot         => '/mnt/www.example.com/docroot/',
+        logroot         => '/mnt/www.example.com/logroot/',
         serveradmin     => 'webmaster@example.com',
         serveraliases   => ['example.com',],
     }
