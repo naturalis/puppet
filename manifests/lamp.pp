7f7ad42d3ff18e022346624fd29c17af8fd68513
root <root@development.(none)>
2013-04-06 14:53:17 +0000

    Add php Add vhost

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -1,3 +1,13 @@
 class { 'motd': }
 class { 'apache': }
+class { 'apache::mod::php': }
 
+apache::vhost { 'www.example.com':
+        priority        => '10',
+        vhost_name      => '192.0.2.1',
+        port            => '8000',
+        docroot         => '/home/www.example.com/docroot/',
+        logroot         => '/srv/www.example.com/logroot/',
+        serveradmin     => 'webmaster@example.com',
+        serveraliases   => ['example.com',],
+    }
