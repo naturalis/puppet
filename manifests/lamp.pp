d286f93708fe8f0bca2f39437a5665d5aec76a03
root <root@dev1.demo-deploy.nl>
2013-04-19 17:22:02 +0200

    Add puppet crontab to lamp

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -2,8 +2,17 @@
 class { 'apache': }
 class { 'apache::mod::php': }
 class { 'apache::mod::rewrite': }
+
 # Create all virtual hosts from hiera
 create_resources('apache::vhost', hiera('virtualhosts', []))
 
 # Create mysql server
 class { 'mysql::server': }
+
+# Crontab example
+cron { 'puppet':
+  ensure  => present,
+  command => '/usr/bin/puppet apply --logdest syslog /etc/puppet/manifests/lamp.pp > /dev/null 2>&1',
+  user    => 'root',
+  minute  => 60,
+}
