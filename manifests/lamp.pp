1c6e79b8b250201cb9966869e706ffbe0c758583
root <root@dev1.demo-deploy.nl>
2013-04-19 17:04:42 +0200

    Add mysql module to lamp stack

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -1,5 +1,9 @@
+# Create apache server, enable php and rewrite mod's
 class { 'apache': }
 class { 'apache::mod::php': }
 class { 'apache::mod::rewrite': }
 # Create all virtual hosts from hiera
 create_resources('apache::vhost', hiera('virtualhosts', []))
+
+# Create mysql server
+class { 'mysql::server': }
