6d99443201de24de2cb0ec3b33f7605c3593d8de
root <root@dev1.demo-deploy.nl>
2013-05-05 14:08:00 +0200

    add monophylizer directory

--- manifests/paulg-monophylizer.pp
+++ manifests/paulg-monophylizer.pp
@@ -10,28 +10,36 @@ create_resources('apache::vhost', hiera('monophylizer', []))
     source   => 'https://github.com/ncbnaturalis/monophylizer.git',
   }
 
-
   file { '/var/monophylizer/script/monophylizer.pl':
     ensure  => 'file',
     mode    => '755',
     require => Vcsrepo['/var/monophylizer'],
   }
 
-  file { '/usr/lib/cgi-bin/monophylizer.pl':
+  file { '/var/www/monophylizer/cgi-bin':
+    ensure => 'directory',
+    mode   => '755',
+    require => Vcsrepo['/var/monophylizer'],
+  }
+
+  file { '/var/www/monophylizer/cgi-bin/monophylizer.pl':
     ensure => 'link',
     mode   => '777',
     target => '/var/monophylizer/script/monophylizer.pl',
+    require => File['/var/www/monophylizer/cgi-bin'],
   }
 
   file { '/var/www/monophylizer/monophylizer.html':
     ensure => 'link',
     mode   => '644',
     target => '/var/monophylizer/html/monophylizer.html',
+    require => Vcsrepo['/var/monophylizer'],
   }
 
   file { '/var/www/monophylizer/sorttable.js':
     ensure => 'link',
     mode   => '644',
     target => '/var/monophylizer/script/sorttable.js',
+    require => Vcsrepo['/var/monophylizer'],
   }
 
