bc0111cdfc38de307fad6f4363414356fcfc97c7
root <root@dev1.demo-deploy.nl>
2013-06-10 22:36:16 +0200

    lint fixups

--- modules/concat/manifests/setup.pp
+++ modules/concat/manifests/setup.pp
@@ -7,31 +7,31 @@
 # $puppetversion should be either 24 or 25 to enable a 24 compatible
 # mode, in 24 mode you might see phantom notifies this is a side effect
 # of the method we use to clear the fragments directory.
-# 
+#
 # The regular expression below will try to figure out your puppet version
 # but this code will only work in 0.24.8 and newer.
 #
 # It also copies out the concatfragments.sh file to /usr/local/bin
 class concat::setup {
-    $root_group = 0
-    $concatdir = "/var/lib/puppet/concat"
-    $majorversion = regsubst($puppetversion, '^[0-9]+[.]([0-9]+)[.][0-9]+$', '\1')
+  $root_group = 0
+  $concatdir = "/var/lib/puppet/concat"
+  $majorversion = regsubst($puppetversion, '^[0-9]+[.]([0-9]+)[.][0-9]+$', '\1')
 
-    file{"/usr/local/bin/concatfragments.sh": 
-            owner  => root,
-            group  => $root_group,
-            mode   => 755,
-            source => $majorversion ? {
-                        24      => "puppet:///concat/concatfragments.sh",
-                        default => "puppet:///modules/concat/concatfragments.sh"
-                      };
+  file{"/usr/local/bin/concatfragments.sh":
+    owner   => root,
+    group   => $root_group,
+    mode    => 755,
+    source  => $majorversion ? {
+      24      => "puppet:///concat/concatfragments.sh",
+      default => "puppet:///modules/concat/concatfragments.sh"
+    };
 
-         $concatdir: 
-            ensure => directory,
-            owner  => root,
-            group  => $root_group,
-            mode   => 755;
-    }
+  $concatdir:
+    ensure => directory,
+    owner  => root,
+    group  => $root_group,
+    mode   => 755;
+  }
 }
 
 # vi:tabstop=4:expandtab:ai
