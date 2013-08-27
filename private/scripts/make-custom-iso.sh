77bacbf9deda9885f43aad6615112491da782bd0
Paul Gomersbach <p.gomersbach@gmail.com>
2013-08-27 16:56:32 +0200

    remove preseed files

--- private/scripts/make-custom-iso.sh
+++ private/scripts/make-custom-iso.sh
@@ -15,12 +15,17 @@ cat > ./custom-iso/isolinux/menu.cfg << EOF
 label custom1
 menu label ^Install Ubuntu 12.04, apply puppet manifest 'puppetdev'
 kernel /install/vmlinuz
-append url=http://raw.github.com/naturalis/puppet/master/private/scripts/preseed-ubuntu-puppetdev.seed initrd=/install/initrd.gz locale=en_US auto=true netcfg/get_hostname=puppetdev console-setup/ask_detect=false keyboard-configuration/layoutcode=us
+append url=http://devops2.googlecode.com/git/files/preseed-ubuntu-puppetdev.seed initrd=/install/initrd.gz locale=en_US auto=true netcfg/get_hostname=puppetdev console-setup/ask_detect=false keyboard-configuration/layoutcode=us
 
 label custom2
 menu label ^Install Ubuntu 12.04, apply puppet manifest 'base'
 kernel /install/vmlinuz
-append url=http://raw.github.com/naturalis/puppet/master/private/scripts/preseed-ubuntu-base.seed initrd=/install/initrd.gz locale=en_US auto=true netcfg/get_hostname=base console-setup/ask_detect=false keyboard-configuration/layoutcode=us
+append url=http://devops2.googlecode.com/git/files/preseed-ubuntu-base.seed initrd=/install/initrd.gz locale=en_US auto=true netcfg/get_hostname=base console-setup/ask_detect=false keyboard-configuration/layoutcode=us
+
+label custom3
+menu label ^Install Ubuntu 12.04, apply puppet manifest 'monophylizer'
+kernel /install/vmlinuz
+append url=http://devops2.googlecode.com/git/files/preseed-ubuntu-monophylizer.seed initrd=/install/initrd.gz locale=en_US auto=true netcfg/get_hostname=monophylizer console-setup/ask_detect=false keyboard-configuration/layoutcode=us
 EOF
 
 mkisofs -R -J -l -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4 -boot-info-table -z -iso-level 4 -c isolinux/isolinux.cat -o ubuntu-12.04.3-custom-amd64.iso custom-iso/
