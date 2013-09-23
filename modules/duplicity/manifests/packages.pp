968787642676f93c29dc90e4eaa155a8aa2240d1
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2013-09-23 10:39:23 +0200

    update duplicity OS compatibility and ftpserver backup functionality

--- modules/duplicity/manifests/packages.pp
+++ modules/duplicity/manifests/packages.pp
@@ -1,6 +1,40 @@
 class duplicity::packages {
   # Install the packages
-  package {
-    ['duplicity', 'python-boto', 'gnupg', 'python-rackspace-cloudfiles']: ensure => present
+
+  case $operatingsystem {
+    centos: {
+      file { "/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL6":
+        owner => root, 
+        group => root, 
+        mode => 0444,
+        source => "puppet:///modules/duplicitystandalone/RPM-GPG-KEY-EPEL6"
+      }
+    
+      yumrepo { "epel":
+        mirrorlist => 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch',
+        enabled => 1,
+        gpgcheck => 1,
+        gpgkey => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL6",
+        require => File["/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL6"]
+      }
+      
+      package {['python-boto','perl-GnuPG','python-cloudfiles','ruby-augeas','PackageKit-cron','duplicity','s3cmd']: 
+          ensure 	=> present,
+          require    	=> Yumrepo['epel'],
+      }
+     }
+    redhat: {
+      package {
+        ['duplicity','python-boto','perl-GnuPG','python-cloudfiles','ruby-augeas','s3cmd']: ensure => present
+      }
+     }
+    debian, ubuntu: {
+      package {
+        ['duplicity','python-boto','gnupg','python-rackspace-cloudfiles','libaugeas-ruby']: ensure => present
+      }
+    }
+    default: {
+      fail("Unrecognized operating system")
+    }
   }
 }
