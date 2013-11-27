926313316e3903958dff0ce82925580177bfb3b4
Paul Gomersbach <p.gomersbach@gmail.com>
2013-11-27 10:28:51 +0100

    add initital foreman manifest

---
+++ manifests/foreman.pp
@@ -0,0 +1,52 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/local/bin" ] }
+$puppetmaster = 'foreman.naturalis.nl'
+
+class { '::puppet': }
+class { '::puppet::server': }
+
+class  { 'foreman':
+  foreman_url     => $puppetmaster,
+  unattended      => true,
+  db_type         => 'mysql',
+  oauth_map_users => false,
+  require         => Class[ '::puppet', '::puppet::server' ],
+}
+
+class  { 'foreman_proxy':
+  register_in_foreman => true,
+  ssl_key             => "/var/lib/puppet/ssl/private_keys/${::fqdn}.pem",
+  ssl_cert            => "/var/lib/puppet/ssl/certs/${::fqdn}.pem",
+  ssl_ca              => "/var/lib/puppet/ssl/certs/ca.pem",
+  ssldir              => "/var/lib/puppet/ssl",
+  require             => Class[ 'foreman' ],
+}
+
+exec { 'gen_puppet_keys':
+  command => 'puppet agent --test --noop; sleep 5; /usr/sbin/puppetdb-ssl-setup -f; service puppetdb restart',
+  require => Class[ '::puppet', '::puppet::server', 'foreman_proxy' ],
+}
+
+exec { 'enable_puppet':
+  command => 'sed -i "s/START=no/START=yes/g" /etc/default/puppet',
+  unless  => 'grep "START=yes" /etc/default/puppet',
+  notify  => Service[ 'puppet' ],
+  require => Exec[ 'gen_puppet_keys' ],
+}
+
+package { 'foreman-compute':
+  ensure  => installed,
+  require => Class[ 'foreman' ],
+}
+
+class { 'puppetdb::master::config':
+  require => Exec[ 'gen_puppet_keys' ],
+}
+
+class { 'puppetdb':
+  ssl_listen_address => '0.0.0.0',
+  listen_address     => '0.0.0.0',
+  require            => Class[ 'puppetdb::master::config' ],
+  notify             => Service[ 'puppet' ],
+}
+
+
