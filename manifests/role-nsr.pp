d13b8bc3d07e18e23b276b0758cc7ec1f85aa42d
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2013-12-10 15:34:24 +0100

    added submodule puppet nsr

--- manifests/role-nsr.pp
+++ manifests/role-nsr.pp
@@ -4,16 +4,4 @@
 #
 # Author : Hugo van Duijn
 #
-class { 'nsr':
-    backup              => true,
-    restore             => false,
-    bucket              => 'linuxbackups',
-    bucketfolder        => 'nsr',
-    dest_id             => $dest_id,
-    dest_key            => $dest_key,
-    cloud               => 's3',
-    coderepo            => 'svn://dev2.etibioinformatics.nl/linnaeus_ng/trunk',
-    repotype            => 'svn',
-    userDbName          => 'nsr',
-    appVersion          => '1.0.0',
-}
+  class { 'nsr': }
