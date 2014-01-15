fc2061d72cac39a77702c66a782b4a54919db471
Hugo van Duijn <hugo.vanduijn@naturalis.nl>
2014-01-15 14:54:02 +0100

    update add to foreman scripts

--- private/scripts/make-custom-iso.sh
+++ private/scripts/make-custom-iso.sh
@@ -9,13 +9,14 @@ fi
 mount -o loop ubuntu-12.04.3-server-amd64.iso ./original-iso
 cp -r ./original-iso/* ./custom-iso/
 cp -r ./original-iso/.disk/ ./custom-iso/
+wget https://raw.github.com/naturalis/puppet/master/private/scripts/preseed-default.seed -O ./custom-iso/preseed/seed.seed
 umount ./original-iso/
 
 cat > ./custom-iso/isolinux/menu.cfg << EOF
 label custom1
 menu label ^Install Ubuntu 12.04, apply puppet manifest add to foreman
 kernel /install/vmlinuz
-append url=https://raw.github.com/naturalis/puppet/master/private/scripts/preseed-default.seed initrd=/install/initrd.gz locale=en_US auto=true console-setup/ask_detect=false keyboard-configuration/layoutcode=us
+append file=/cdrom/preseed/seed.seed initrd=/install/initrd.gz locale=en_US auto=true console-setup/ask_detect=false keyboard-configuration/layoutcode=us
 
 # label custom2
 # menu label ^Install Ubuntu 12.04, apply puppet manifest 'base' and add to foreman
